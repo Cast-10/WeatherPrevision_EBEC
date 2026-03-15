ddaede521d0804b971209a957779965e4d482864
ze_pacheco <up202406161@up.pt>
2026-03-15 13:46:39 +0000

    Improved Level1 Model

--- Level1.py
+++ Level1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import HistGradientBoostingClassifier
 from sklearn.metrics import f1_score
 import utils
 import joblib
@@ -28,10 +29,20 @@ def prepare_level1_data(df):
 
 def trainLevel1(X, y):
     # This function now ONLY handles the ML training
-    model = RandomForestClassifier(n_estimators=200, random_state=13, n_jobs=-1)
+    model = HistGradientBoostingClassifier(
+        max_iter=200,              # Similar to n_estimators in Random Forest
+        learning_rate=0.1,         # Learning speed
+        max_depth=10,              # Avoid overfitting
+        l2_regularization=1.5,     # Helps generelization
+        categorical_features=[0],  # 'location' column is categorical
+        class_weight='balanced',   # CRUCIAL: More weight to the 'rain' class (it rains less than it does not rain)
+        random_state=13,
+        min_samples_leaf= 20
+    )
     print("Training Level 1...")
     model.fit(X, y)
     print("Finished training.")
+    
     return model
 
 def testLevel1(df):
